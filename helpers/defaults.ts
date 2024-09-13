22427340a73d5b4ade41fb36b4ebfdd2b1edc4f2
Soontorn Choosenpom <soontornc008@gmail.com>
2024-09-13 09:22:06 +0700

    First commit

---
+++ helpers/defaults.ts
@@ -0,0 +1,17 @@
+import type { DefaultsInstance } from "vuetify"
+export const defaults: DefaultsInstance = {
+    VAppBar: {
+        elevation: 0,
+    },
+    VBtn: {
+        variant: "flat",
+        height: 38,
+        rounded: "lg",
+        size: "small",
+    },
+    VTextField: {
+        color: "primary",
+        variant: "outlined",
+        density: "comfortable",
+    },
+}
---
+++ types/product.d.ts
@@ -0,0 +1,15 @@
+export interface Product {
+    id: number
+    productName: string
+    unitPrice: number
+    unitInStock: number
+    productPicture: string
+    categoryId: number
+    categoryName: string
+    createdDate: string // ISO date string
+    modifiedDate: string | null // ISO date string or null
+  }
+  export interface ProductList {
+    total: number
+    products: Product[]
+  }
